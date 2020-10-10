c6ca06fb1a7f6d364065d972b169384c3c0c93d8
HaifaaNabeel <hauof53@gmail.com>
2020-10-10 11:40:48 +0300

    code record for run js in php

---
+++ function to record/index.php
@@ -0,0 +1,20 @@
+<?PHP 
+ /*echo"<script>
+ document.write('<h1> Hello World </h1> <br> Welcome ^_^ <br>')
+  </script> ";*/
+?>
+<script>
+ 
+  //document.write('<h3> without tag php </h3> <br> <h1> Hello World </h1>  <br> Welcome ^_^ <br>')
+  
+</script>
+
+<?php 
+    echo "<div id='IT'></div>"; 
+?> 
+<script >
+
+var funcVar = '<h3> By using id  </h3> <br> <h1> Hello World </h1>  <br> Welcome ^_^ <br>' ;   
+document.getElementById("IT").innerHTML = funcVar; 
+
+</script>
